5cd8f59c81b8e6a5c8eb608d0149fc46676b2286
Hayaki Saito <user@zuse.jp>
2014-07-27 17:23:03 +0900

    Add license blocks

--- gifsave89.h
+++ gifsave89.h
@@ -1,3 +1,23 @@
+/*
+ * Copyright(c) 2012-2012, John Forkosh Associates, Inc. All rights reserved.
+ *           http://www.forkosh.com   mailto: john@forkosh.com
+ * Copyright (C) 2014 haru <uobikiemukot at gmail dot com>
+ * Copyright (C) 2014 Hayaki Saito <user@zuse.jp>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 /* --- program id --- */
 #define        PROGRAMID     "gifsave89"        /* program name */
 #define        VERSION       "1.00"        /* version number */
