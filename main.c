337ac96ecb67c38dca9732ff19eee12f0a46ef53
Koichi Murase <myoga.murase@gmail.com>
2019-03-08 09:25:48 +0900

    Fix a bug that the last frame is not output

--- main.c
+++ main.c
@@ -632,7 +632,14 @@ int main(int argc, char *argv[])
     }
 
     if (settings.last_frame_delay > 0) {
-        controlgif(gsdata, -1, settings.last_frame_delay / 10, 0, 0);
+        if (nret != EXIT_FAILURE) {
+            refresh(&pb, &term);
+            apply_colormap(&pb, img);
+        }
+        delay = settings.last_frame_delay / 10;
+        if (delay < gif_render_interval)
+            delay = gif_render_interval;
+        controlgif(gsdata, -1, delay, 0, 0);
         putgif(gsdata, img);
     }
 
